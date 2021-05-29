fa6d14d1de0f2353ba99840efca1859c5d33e49f
Davidson Ratis <davidsonmakarios@gmail.com>
2021-05-28 21:35:13 -0300

    page header and footer

---
+++ templates/CrudFooter.php
@@ -0,0 +1,5 @@
+    <!-- My Scripts -->
+    <script src="./public/js/form-jquery.js"></script>
+    <script src="./public/js/modal-jquery.js"></script>
+  </body>
+</html>
---
+++ templates/CrudHeader.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>CRUD com PDO</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="public/css/home-styles.css">
+    <link rel="stylesheet" href="public/css/modal-styles.css">
+    <link rel="stylesheet" href="public/css/form-styles.css">
+    <link rel="stylesheet" href="public/css/error-pages.css">
+    <!-- Scripts -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.3.0/jquery.form.min.js" 
+      integrity="sha384-qlmct0AOBiA2VPZkMY3+2WqkHtIQ9lSdAsAn5RUJD/3vA5MKDgSGcdmIv4ycVxyn" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+  </head>
+  <body>
